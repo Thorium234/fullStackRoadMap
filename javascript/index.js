c26be08888b974cab7e51649609686baf5742379
Thorium234 <inyangalapaul5@gmail.com>
2026-05-14 22:02:15 +0300

    added material for documentation

--- javascript/index.js
+++ javascript/index.js
@@ -21,4 +21,13 @@ console.log(message)
 //a constant is a variable that doesent change its value as long as  is runing
 //
 const FILE_SIZE_LIMIT= 5000
+//
+//
+//Exercise #2
+//write a program with three variables
+//
+let name="thorium234"
+let age = 24
+let occupation = "student"
 
+console.log("my name is "+ name"" +"" age+"" occupation)
