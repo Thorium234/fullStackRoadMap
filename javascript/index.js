13bcd63e8be200d0e1786c0cbc9c1edf067a99a3
Thorium234 <inyangalapaul5@gmail.com>
2026-05-14 21:15:33 +0300

    javascipt basics

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+//this is a comment
+//
+//exercise #1
+//try to print your name,age and ocupation on the console
+//
+console.log("thorium234");
+console.log(24);
+console.log("student");
+
+//javascript variables
+//
+let message = "hello world"
+console.log(message)
+
+//variables naming
+//1. camelCase
+//2. snake_case
+//
+//constant variables
+//a constant is a variable that doesent change its value as long as  is runing
+//
+const FILE_SIZE_LIMIT= 5000
+
