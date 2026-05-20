8de9014933a6663482a83fbe65a62e78fd8e4511
Thorium234 <inyangalapaul5@gmail.com>
2026-05-21 00:13:26 +0300

    Building a quiz app with HTML, CSS, and JavaScript requires a combination of structural layout, visual design, and interactive logic. At a minimum, you need a code editor like Visual Studio Code and a modern web browser to test your work

---
+++ quizApp/quiz.js
@@ -0,0 +1,18 @@
+//accessing quesions
+let question = document.getElementById('quiz1');
+
+let answer1 = document.getElementById('answ1');
+let answer2= document.getElementById('answ2');
+let answer3 = document.getElementById('answ3');
+
+let complement = document.getElementById('remarks');
+
+if (answer1===true) {
+    // complement.textContent = "Wow we did it again";
+    console.log("true");
+}else{
+    console.log("false");
+    
+}
+
+    console.log("true");
