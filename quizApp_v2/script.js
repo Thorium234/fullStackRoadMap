7f3fe30284de793f498e68bde05dd88398be3b05
Thorium234 <inyangalapaul5@gmail.com>
2026-05-21 23:16:05 +0300

    added well structured simple if statement

---
+++ quizApp_v2/script.js
@@ -0,0 +1,19 @@
+function submitQuiz(){
+    let score = 0;
+    //1. get the selected values(return null if not selected)
+    let q1 = document.querySelector('input[name="q1"]:checked');
+    let q2 = document.querySelector('input[name="q2"]:checked');
+    let q3 = document.querySelector('input[name="q3"]:checked');
+    let q4 = document.querySelector('input[name="q4"]:checked');
+    let q5 = document.querySelector('input[name="q5"]:checked');
+
+    //2. check the answers and update the score
+    if(q1 && q1.value === 'a') score++;
+    if(q2 && q2.value === 'b') score++;
+    if(q3 && q3.value === 'b') score++;
+    if(q4 && q4.value === 'c') score++;
+    if(q5 && q5.value === 'b') score++;
+
+    //3. display the results
+    document.querySelector('.quizResult').textContent = `Your score is: ${score}/5`;
+}
