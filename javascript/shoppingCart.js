ad175510c070f50b4c216d0bb52415421264cd96
Thorium234 <inyangalapaul5@gmail.com>
2026-05-25 20:43:28 +0300

    Project 2: Simple Shopping Cart
    Goal: Calculate total price with tax and discount.

---
+++ javascript/shoppingCart.js
@@ -0,0 +1,11 @@
+const cart = [
+  { item: "Shirt", price: 800 },
+  { item: "Pants", price: 1200 },
+  { item: "Shoes", price: 1500 }
+];
+function calculateTotal(cartItems) {
+  const taxed = cartItems.map(p => p.price * 1.18);
+  const total = taxed.reduce((sum, price) => sum + price, 0);
+  return `Total after 18% tax: ₹${total.toFixed(2)}`;
+}
+console.log(calculateTotal(cart));
