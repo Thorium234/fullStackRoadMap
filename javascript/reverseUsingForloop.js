b02d8170cd309ee4bf5db3fac3fd59b87818af19
Thorium234 <inyangalapaul5@gmail.com>
2026-05-31 21:17:02 +0300

    added reverse str using for loop

---
+++ javascript/reverseUsingForloop.js
@@ -0,0 +1,11 @@
+function reverseWordsForLoop(str) {
+  let newString = ""
+ 
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
+
+console.log(reverseWordsForLoop("loop"));
+//reverseWordsForLoop(“loop”)
